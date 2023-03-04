b864d4a89c313d637173220e4b047fca58008536
Mano Ségransan <mano.segransan@protonmail.com>
2023-03-04 17:50:35 +0100

    Add 6x6 and 7x7, update to gradle 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,32 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-  kotlin("jvm") version "1.6.10"
-  application
-  id("com.github.johnrengelman.shadow") version "6.1.0"
+    kotlin("jvm") version "1.8.10"
+    application
+    id("com.github.johnrengelman.shadow") version "8.1.0"
 }
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-  implementation("org.worldcubeassociation.tnoodle:lib-scrambles:0.18.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-  implementation("org.jetbrains.kotlinx:atomicfu-jvm:0.17.2")
-  implementation("com.github.ajalt.clikt:clikt:3.4.2")
-  implementation("org.mongodb:mongodb-driver-sync:4.6.0")
-  implementation("com.google.code.gson:gson:2.9.0")
+    implementation("org.worldcubeassociation.tnoodle:lib-scrambles:0.18.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
+    implementation("org.jetbrains.kotlinx:atomicfu-jvm:0.17.2")
+    implementation("com.github.ajalt.clikt:clikt:3.4.2")
+    implementation("org.mongodb:mongodb-driver-sync:4.6.0")
+    implementation("com.google.code.gson:gson:2.9.0")
 }
 
-tasks {
-  withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
-  shadowJar {
-    archiveClassifier.set("")
-  }
+kotlin {
+    jvmToolchain(19)
 }
 
 application {
-  mainClassName = "MainKt"
+    mainClass.set("MainKt")
+}
+
+tasks {
+    shadowJar {
+        archiveClassifier.set("")
+    }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,59 +8,61 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>) {
-	val mongoClient = MongoClients.create("mongodb://${args[0]}:${args[1]}/")
-	val database = mongoClient.getDatabase(args[2])
-	val scrambleStringCollection = database.getCollection("scrambles")
-	val scrambleSvgCollection = database.getCollection("svgs")
+    val mongoClient = MongoClients.create("mongodb://${args[0]}:${args[1]}/")
+    val database = mongoClient.getDatabase(args[2])
+    val scrambleStringCollection = database.getCollection("scrambles")
+    val scrambleSvgCollection = database.getCollection("svgs")
 
 	val date = LocalDate.now().plusDays(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
 
-	val puzzles = mapOf(
-		"333" to PuzzleRegistry.THREE,
-		"222" to PuzzleRegistry.TWO,
-		"444" to PuzzleRegistry.FOUR,
-		"555" to PuzzleRegistry.FIVE,
-		"3BLD" to PuzzleRegistry.THREE_NI,
-		"OH" to PuzzleRegistry.THREE,
-		"SQ1" to PuzzleRegistry.SQ1,
-		"MEGA" to PuzzleRegistry.MEGA,
-		"PYRA" to PuzzleRegistry.PYRA,
-		"CLOCK" to PuzzleRegistry.CLOCK,
-		"SKEWB" to PuzzleRegistry.SKEWB,
-	)
+    val puzzles = mapOf(
+        "333" to PuzzleRegistry.THREE,
+        "222" to PuzzleRegistry.TWO,
+        "444" to PuzzleRegistry.FOUR,
+        "555" to PuzzleRegistry.FIVE,
+        "3BLD" to PuzzleRegistry.THREE_NI,
+        "OH" to PuzzleRegistry.THREE,
+        "SQ1" to PuzzleRegistry.SQ1,
+        "MEGA" to PuzzleRegistry.MEGA,
+        "PYRA" to PuzzleRegistry.PYRA,
+        "CLOCK" to PuzzleRegistry.CLOCK,
+        "SKEWB" to PuzzleRegistry.SKEWB,
+        "666" to PuzzleRegistry.SIX,
+        "777" to PuzzleRegistry.SEVEN,
+    )
 
-	puzzles.forEach { (t, u) -> generateScrambles(t, u, date, scrambleStringCollection, scrambleSvgCollection) }
+    puzzles.forEach { (t, u) -> generateScrambles(t, u, date, scrambleStringCollection, scrambleSvgCollection) }
 }
 
 fun generateScrambles(
-	event: String,
-	Puzzle: PuzzleRegistry,
-	date: String,
-	scrambleStringCollection: MongoCollection<Document>,
-	scrambleSvgCollection: MongoCollection<Document>
+    event: String,
+    puzzle: PuzzleRegistry,
+    date: String,
+    scrambleStringCollection: MongoCollection<Document>,
+    scrambleSvgCollection: MongoCollection<Document>
 ) {
-	val scrambles = getScrambleString(Puzzle, 5)
-	scrambleStringCollection.insertOne(Document.parse(Gson().toJson(ScrambleStringObject(scrambles, event, date))))
-	if (event !in arrayOf("333", "222", "OH", "3BLD", "444", "555", "MEGA")) {
-		val svg = getScrambleSvg(Puzzle, scrambles)
-		scrambleSvgCollection.insertOne(Document.parse(Gson().toJson(ScrambleSvgObject(svg, event, date))))
-	}
+    val scrambles = getScrambleString(puzzle, 5)
+    scrambleStringCollection.insertOne(Document.parse(Gson().toJson(ScrambleStringObject(scrambles, event, date))))
+    if (event in arrayOf("SKEWB", "CLOCK", "PYRA", "SQ1")) {
+        val svg = getScrambleSvg(puzzle, scrambles)
+        scrambleSvgCollection.insertOne(Document.parse(Gson().toJson(ScrambleSvgObject(svg, event, date))))
+    }
 }
 
 fun parseSVG(svg: Svg): String {
-	svg.setAttribute("width", "100%")
-	svg.setAttribute("height", "100%")
-	return svg.toString().replace(oldValue = "stroke=\"#000000\"", newValue = "stroke=\"#1E1E1E\"")
+    svg.setAttribute("width", "100%")
+    svg.setAttribute("height", "100%")
+    return svg.toString().replace(oldValue = "stroke=\"#000000\"", newValue = "stroke=\"#1E1E1E\"")
 }
 
 class ScrambleStringObject(val scrambles: List<String>, val event: String, val date: String)
 
-fun getScrambleString(Puzzle: PuzzleRegistry, n: Int): List<String> {
-	return List(n) { Puzzle.scrambler.generateScramble() }
+fun getScrambleString(puzzle: PuzzleRegistry, n: Int): List<String> {
+    return List(n) { puzzle.scrambler.generateScramble() }
 }
 
 class ScrambleSvgObject(val svg: List<String>, val event: String, val date: String)
 
-fun getScrambleSvg(Puzzle: PuzzleRegistry, scrambles: List<String>): List<String> {
-	return scrambles.map { s -> parseSVG(Puzzle.scrambler.drawScramble(s, Puzzle.scrambler.defaultColorScheme)) }
+fun getScrambleSvg(puzzle: PuzzleRegistry, scrambles: List<String>): List<String> {
+    return scrambles.map { s -> parseSVG(puzzle.scrambler.drawScramble(s, puzzle.scrambler.defaultColorScheme)) }
 }
